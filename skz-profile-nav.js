896e99dd723597990d63905a600ffa06b55f765f
CRUSH <107373418+crush-ing@users.noreply.github.com>
2022-06-26 03:23:30 -0500

    update

--- skz-profile-nav.js
+++ skz-profile-nav.js
@@ -1,8 +1,8 @@
-document.write('<a href="#bangchan"><img width="110" height="500" loading="lazy" src="assets/skz/profiles/profile-1-bang-chan-pic.png"></a>');
-document.write('<a href="#leeknow"><img width="110" height="500" loading="lazy" src="assets/skz/profiles/profile-2-lee-know-pic.png"></a>');
-document.write('<a href="#changbin"><img width="110" height="500" loading="lazy" src="assets/skz/profiles/profile-3-changbin-pic.png"></a>');
-document.write('<a href="#hyunjin"><img width="110" height="500" loading="lazy" src="assets/skz/profiles/profile-4-hyunjin-pic.png"></a>');
-document.write('<a href="#han"><img width="110" height="500" loading="lazy" src="assets/skz/profiles/profile-5-han-pic.png"></a>');
-document.write('<a href="#felix"><img width="110" height="500" loading="lazy" src="assets/skz/profiles/profile-6-felix-pic.png"></a>');
-document.write('<a href="#seungmin"><img width="110" height="500" loading="lazy" src="assets/skz/profiles/profile-7-seungmin-pic.png"></a>');
-document.write('<a href="#in"><img width="110" height="500" loading="lazy" src="assets/skz/profiles/profile-8-in-pic.png"></a>');
+document.write('<a href="#bangchan"><img width="110" height="135" loading="lazy" src="assets/skz/profiles/profile-1-bang-chan-pic.png"></a> ');
+document.write('<a href="#leeknow"><img width="110" height="135" loading="lazy" src="assets/skz/profiles/profile-2-lee-know-pic.png"></a> ');
+document.write('<a href="#changbin"><img width="110" height="135" loading="lazy" src="assets/skz/profiles/profile-3-changbin-pic.png"></a> ');
+document.write('<a href="#hyunjin"><img width="110" height="135" loading="lazy" src="assets/skz/profiles/profile-4-hyunjin-pic.png"></a> ');
+document.write('<a href="#han"><img width="110" height="135" loading="lazy" src="assets/skz/profiles/profile-5-han-pic.png"></a> ');
+document.write('<a href="#felix"><img width="110" height="135" loading="lazy" src="assets/skz/profiles/profile-6-felix-pic.png"></a> ');
+document.write('<a href="#seungmin"><img width="110" height="135" loading="lazy" src="assets/skz/profiles/profile-7-seungmin-pic.png"></a> ');
+document.write('<a href="#in"><img width="110" height="135" loading="lazy" src="assets/skz/profiles/profile-8-in-pic.png"></a>');
