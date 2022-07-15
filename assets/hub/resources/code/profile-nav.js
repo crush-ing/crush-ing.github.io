1847cfb69251bac2a078c9daa2b8138ce6feb00e
CRUSH <107373418+crush-ing@users.noreply.github.com>
2022-07-14 19:55:46 -0500

    update

---
+++ assets/hub/resources/code/profile-nav.js
@@ -0,0 +1,8 @@
+document.write('<a href="#profile-1"><img width="110" height="135" loading="lazy" src="https://picsum.photos/401/501"></a> ');
+document.write('<a href="#profile-2"><img width="110" height="135" loading="lazy" src="https://picsum.photos/402/502"></a> ');
+document.write('<a href="#profile-3"><img width="110" height="135" loading="lazy" src="https://picsum.photos/403/503"></a> ');
+document.write('<a href="#profile-4"><img width="110" height="135" loading="lazy" src="https://picsum.photos/404/504"></a> ');
+document.write('<a href="#profile-5"><img width="110" height="135" loading="lazy" src="https://picsum.photos/405/505"></a> ');
+document.write('<a href="#profile-6"><img width="110" height="135" loading="lazy" src="https://picsum.photos/406/506"></a> ');
+document.write('<a href="#profile-7"><img width="110" height="135" loading="lazy" src="https://picsum.photos/407/507"></a> ');
+document.write('<a href="#profile-8"><img width="110" height="135" loading="lazy" src="https://picsum.photos/408/508"></a>');
